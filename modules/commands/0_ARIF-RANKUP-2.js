c900e6f01737f13963ce9f0a02c30d104ec6c354
ARIFBABU78600 <git.up2@yandex.ru>
2024-08-09 08:16:41 +0530

    Update 0_ARIF-RANKUP-2.js

--- modules/commands/0_ARIF-RANKUP-2.js
+++ modules/commands/0_ARIF-RANKUP-2.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-	name: "rankup",
-	version: "7.3.1",
+	name: "PREM-RANKUP-2",
+	version: "3.3.1",
 	hasPermssion: 1,
-	credits: "Gudiya",
-	description: "Announce rankup for each group, user",
-	commandCategory: "Edit-IMG",
+	credits: "PREM BABU",
+	description: "THIS BOT IS MADE BY PREM BABU",
+	commandCategory: "LEVAL UP",
 	dependencies: {
 		"fs-extra": ""
 	},
@@ -52,7 +52,9 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
 		const moduleName = this.config.name;
 
     var background = [
-  "https://i.imgur.com/rfDdAPX.jpg","https://i.imgur.com/A4Eq1YY.jpg"];
+  "https://i.imgur.com/NIgGboD.jpeg"
+      
+  ];
     var rd = background[Math.floor(Math.random() * background.length)];
     let getAvtmot = (
     await axios.get(
@@ -74,8 +76,8 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
     let canvas = createCanvas(baseImage.width, baseImage.height);
     let ctx = canvas.getContext("2d");
     ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.rotate(-25 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 70, 700, 635, 700);
+    ctx.rotate(-0 * Math.PI / 180);
+    ctx.drawImage(baseAvt1, 2290, 610, 700, 700);
     const imageBuffer = canvas.toBuffer();
     fs.writeFileSync(pathImg, imageBuffer);
     fs.removeSync(pathAvt1);
@@ -88,17 +90,11 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
 }
 
 module.exports.languages = {
-	"vi": {
-		"off": "𝗧𝗮̆́𝘁",
-		"on": "𝗕𝗮̣̂𝘁",
-		"successText": "𝐭𝐡𝐚̀𝐧𝐡 𝐜𝐨̂𝐧𝐠 𝐭𝐡𝐨̂𝐧𝐠 𝐛𝐚́𝐨 𝐫𝐚𝐧𝐤𝐮𝐩 ✨",
-		"levelup": "🌸 𝗞𝗶̃ 𝗻𝗮̆𝗻𝗴 𝘅𝗮̣𝗼 𝗹𝗼̂̀𝗻𝗻 𝗼̛̉ 𝗺𝗼̂𝗻 𝗽𝗵𝗮́𝗽 𝗵𝗮̂́𝗽 𝗱𝗶𝗲̂𝗺 𝗰𝘂̉𝗮 {name} 𝘃𝘂̛̀𝗮 𝗹𝗲̂𝗻 𝘁𝗼̛́𝗶 𝗹𝗲𝘃𝗲𝗹 {level} 🌸"
-	},
 	"en": {
 		"on": "on",
 		"off": "off",
 		"successText": "success notification rankup!",
-		"levelup": "{name}, 𒁍 बधाई हो आपका लेवल [{level}] हो गया 😐🙈",
+		"levelup": "{name} 𒁍 बधाई हो आपका लेवल [ {level} ] हो गया 🐥🙈🌺",
 	}
 }
 
@@ -112,4 +108,4 @@ module.exports.run = async function({ api, event, Threads, getText }) {
 	await Threads.setData(threadID, { data });
 	global.data.threadData.set(threadID, data);
 	return api.sendMessage(`${(data["rankup"] == true) ? getText("on") : getText("off")} ${getText("successText")}`, threadID, messageID);
-             }
+                            }
