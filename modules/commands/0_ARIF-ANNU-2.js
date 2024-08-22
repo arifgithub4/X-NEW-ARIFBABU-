b99b3757c2da5d1a71cbe03514cc4a74657f8ce7
arifgithub4 <arifgithub4@yandex.com>
2024-08-22 18:27:40 +0530

    Update 0_ARIF-ANNU-2.js

--- modules/commands/0_ARIF-ANNU-2.js
+++ modules/commands/0_ARIF-ANNU-2.js
@@ -71,6 +71,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "❥︎|====『  𝗟❤︎𝗩𝗲  』====|☻︎", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "❥︎|====『  𝗟♥️𝗩𝗲  』====|☻︎", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
       }
