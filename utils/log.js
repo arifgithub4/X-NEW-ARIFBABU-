093a989125d21a02fa8c60830254e3e5180d7ef1
arifgithub4 <arifgithub4@yandex.com>
2024-08-24 08:44:00 +0530

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -19,19 +19,19 @@ job[Math.floor(Math.random() * job.length)]
 module.exports.loader = (data, option) => {
 	switch (option) {   
 		case "success":
-			console.log(chalk.bold.hex("#00FF00").bold('PREM PROJECT ❯ ') + data);
+			console.log(chalk.bold.hex("#00FF00").bold('ARIF BABU ❯ ') + data);
 			break;
 		case "warn":
-			console.log(chalk.bold.hex("#FF0000").bold('PREM PROJECT ❯ ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('ARIF BABU ❯ ') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FF0000").bold('PREM PROJECT ❯ ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('ARIF BABU ❯ ') + data);
 			break;
 		default:
       var job = ["#FFA500","#FFFF00","#00FF00","#0000FF", "#4B0082","#A020F0"];
     var random = 
 job[Math.floor(Math.random() * job.length)]
-			console.log(chalk.bold.hex(random).bold(`PREM PROJECT ❯ `) + data);
+			console.log(chalk.bold.hex(random).bold(`ARIF BABU ❯ `) + data);
 			break;
 	}
                   }
